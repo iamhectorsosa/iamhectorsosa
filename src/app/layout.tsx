b670c66ed48a15d826aee84a7decb239ab912377
Hector Sosa <hello@hectorsosa.me>
2023-07-25 14:30:59 +0200

    Updating Grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,18 @@ export default function RootLayout({
       <body
         className={`${grotesk.variable} ${mono.variable} font-sans bg-neutral-50 text-neutral-900 max-w-3xl mx-auto px-4`}
       >
+        <div
+          style={{
+            position: "absolute",
+            width: "1200px",
+            height: "630px",
+            top: "-38.5%",
+            left: "50%",
+            opacity: "0.5",
+            backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%239C92AC' fill-opacity='0.29'%3E%3Cpath d='M36 34v-4h-2v4h-4v2h4v4h2v-4h4v-2h-4zm0-30V0h-2v4h-4v2h4v4h2V6h4V4h-4zM6 34v-4H4v4H0v2h4v4h2v-4h4v-2H6zM6 4V0H4v4H0v2h4v4h2V6h4V4H6z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
+          }}
+        ></div>
+
         <Navbar />
         <main className="py-12">{children}</main>
         <Footer />
